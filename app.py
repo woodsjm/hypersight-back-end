505142196ebda60c14ec346e31b8e052d10b4b9c
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-09-10 09:53:35 -0500

    code

--- app.py
+++ app.py
@@ -33,20 +33,20 @@ app.secret_key = 'RLAKJDRANDOMASDFLKENCASDFWERACSVNASDFLKJQWEFASDF STRING'
 
 CORS(app, origins=['http://localhost:3000', 'https://hypersight.herokuapp.com'], supports_credentials=True)
 
-# ----------------RETURN APPROPRIATE HEADERS ON EVERY REQUEST TO SERVER----------------
-@app.after_request
-def add_cors(resp):
+# # ----------------RETURN APPROPRIATE HEADERS ON EVERY REQUEST TO SERVER----------------
+# @app.after_request
+# def add_cors(resp):
     
-    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
-    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get( 
-        'Access-Control-Request-Headers', 'Authorization' )
+#     resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+#     resp.headers['Access-Control-Allow-Credentials'] = 'true'
+#     resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+#     resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get( 
+#         'Access-Control-Request-Headers', 'Authorization' )
     
-    if app.debug:
-        resp.headers['Access-Control-Max-Age'] = '1'
+#     if app.debug:
+#         resp.headers['Access-Control-Max-Age'] = '1'
 
-    return resp
+#     return resp
 
 
 
