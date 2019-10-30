40df7e307c41f75afa4a6deafcf324fb638f1b23
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2019-10-30 04:15:30 -0500

    Refactored code

--- app.py
+++ app.py
@@ -45,6 +45,23 @@ app.secret_key = 'RLAKJDRANDOM STRING'
 
 CORS(app, origins=['http://localhost:3000', 'https://hypersight.herokuapp.com'], supports_credentials=True)
 
+# -------------------------------------------
+#               Helper Functions
+# -------------------------------------------
+
+def get_file_name_references(current_user):
+    file_name_references = mongo.db.users.find({
+        'username': current_user
+        }, {'files': 1})
+    return file_name_references
+
+def make_file_name_references_list(file_refs):
+    file_name_references_list = []
+    for element in file_refs:
+        file_name_references_list.append(element['files'])
+    return file_name_references_list
+
+
 
 # ----------------LOG USER IN----------------
 @app.route('/login', methods=["POST"])
